e3ca1f5f6a4a10d4b872caeacba804889e6b0310
Jose Rubio <jarb29@example.com>
2025-02-18 16:47:00 -0300

    Add missing newline before return statement
    A newline was added before the return statement in `util_functions.py` for improved code readability. This change has no functional impact but enhances code style consistency.

--- util_functions.py
+++ util_functions.py
@@ -196,6 +196,7 @@ def group_and_sum_without_remove_columns(df, group_columns, avg_column,     cols
     })
     grouped_df['mm de perforado'] = grouped_df['Total de Perforaciones'] * grouped_df['espesor']
     grouped_df.reset_index(inplace=True)
+
     return grouped_df
 
 
